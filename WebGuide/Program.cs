2297a43451577a07a0c29dba1a93d715ac8b66c6
Kinosura666 <kornievskiy.leha@gmail.com>
2025-04-14 22:17:20 +0300

    config upd

--- WebGuide/Program.cs
+++ WebGuide/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+CustomFontResolver.Register();
 builder.Services.AddHttpClient<TrelloService>();
 builder.Services.AddSingleton<GoogleCloudStorageService>();
 builder.Services.AddHttpClient<MailjetEmail>(); 
